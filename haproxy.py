3d518c5b63617bae267d02e04ef37d64495ff220
Shunya Kitada <syun.kitada@gmail.com>
2016-02-20 19:11:38 +0900

    update

--- haproxy.py
+++ haproxy.py
@@ -27,7 +27,7 @@ class Haproxy(SimpleBase):
         }
 
     def init_after(self):
-        for cluster in self.data.get('clusters', {}).values():
+        for cluster in self.data.get('cluster_map', {}).values():
             if env.host in cluster['hosts']:
                 self.data.update(cluster)
                 break
